7e774d716377a10c7b469ad17f63b69e68192ac7
RajCodeLabs <44139208+RajCodeLabs@users.noreply.github.com>
2023-10-31 16:03:35 +0530

    Time: 56 ms (77.84%), Space: 13.6 MB (50.28%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -1,23 +1,35 @@
 class Solution {
 public:
-    vector<vector<int>> fourSum(vector<int> &nums, int target) {
-        unordered_set<long long> seen;
-        set<multiset<long long>> ans_set;
-        int n = size(nums);
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                for (int k = j + 1; k < n; k++) {
-                    long long lastNumber = (long long) target - ((long long) nums[i] + (long long) nums[j] + (long long) nums[k]);
-                    if (seen.count(lastNumber))
-                        ans_set.emplace(multiset<long long>{nums[i], nums[j], nums[k], lastNumber});
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        int i, j, len = nums.size();
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> output;
+        for(i = 0; i < (len - 3); i++){
+            for(j = (i + 1); j < (len - 2); j++){
+                long long newTarget = (long long)target - (long long)nums[i] - (long long)nums[j];
+                int low = j + 1, high = len - 1;
+                while(low < high){
+                    if((nums[low] + nums[high]) < newTarget){
+                        low++;
+                    }
+                    else if((nums[low] + nums[high]) > newTarget){
+                        high--;
+                    }
+                    else{
+                        output.push_back({nums[i], nums[j], nums[low], nums[high]});
+                        int tempIndex1 = low, tempIndex2 = high;
+                        while((low < high) && (nums[low] == nums[tempIndex1]))
+                            low++;
+                        while((low < high) && (nums[high] == nums[tempIndex2]))
+                            high--;
+                    }
                 }
+                while(((j + 1) < len) && (nums[j] == nums[j + 1]))
+                    j++;
             }
-            seen.insert(nums[i]);
+            while(((i + 1) < len) && (nums[i] == nums[i + 1]))
+                i++;
         }
-        vector<vector<int>> ans;
-        for (const auto &el : ans_set) {
-            ans.emplace_back(vector<int>(begin(el), end(el)));
-        }
-        return ans;
+        return output;
     }
 };
